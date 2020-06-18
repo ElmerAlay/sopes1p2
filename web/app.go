1ba6cf7f17a0d02c967172efbbb1d73f6bc4b254
ElmerAlay <alayelmer1993@gmail.com>
2020-06-18 12:24:31 -0600

    add and get persons

--- web/app.go
+++ web/app.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -22,11 +24,28 @@ var persons []Person
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 
-	router.HandleFunc("/", index)
+	router.HandleFunc("/", index).Methods("GET")
+	router.HandleFunc("/", addPerson).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":3000", router))
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World!")
+	//w.Header().Set("Content-Type", "applycation/json")
+	json.NewEncoder(w).Encode(persons)
+}
+
+func addPerson(w http.ResponseWriter, r *http.Request) {
+	var person Person
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Something wrong has passed")
+	}
+
+	json.Unmarshal(reqBody, &person)
+	persons = append(persons, person)
+
+	w.Header().Set("Content-Type", "applycation/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(person)
 }
