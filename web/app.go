998c47f62ff798670e026f939f13554321c44af1
ElmerAlay <alayelmer1993@gmail.com>
2020-06-18 11:57:27 -0600

    welcome message defined in main page

--- web/app.go
+++ web/app.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Person : estructura que almacenará el dato recibido o enviado
+type Person struct {
+	Name      string `json:"Nombre"`
+	Depto     string `json:"Departamento"`
+	Age       int    `json:"Edad"`
+	Contagion string `json:"Forma de contagio"`
+	State     string `json:"Estado"`
+}
+
+var persons []Person
 
 func main() {
-	fmt.Println("Hello World!")
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/", index)
+
+	log.Fatal(http.ListenAndServe(":3000", router))
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World!")
 }
