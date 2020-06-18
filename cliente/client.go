4cdc8ba06c55c2c1e5342634ff9fe992af7ddb6f
ElmerAlay <alayelmer1993@gmail.com>
2020-06-18 13:27:56 -0600

    connecting client with server

--- cliente/client.go
+++ cliente/client.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"sync"
 	"time"
 )
@@ -22,12 +24,15 @@ var num int
 
 func main() {
 	// path : ruta del archivo a leer
-	var path string
+	// url : la ruta a la que deseamos enviar
+	var path, url string
 
 	// threads : cantidad de hilos a utilizar para enviar
 	// amount : cantidad de datos del archivo que queremos enviar
 	var threads, amount int
 
+	fmt.Printf("Ingresa la url a dónde deseeas enviar los datos: ")
+	fmt.Scanf("%s\n", &url)
 	fmt.Printf("Ingresa la cantidad de hilos que desea utilizar: ")
 	fmt.Scanf("%d\n", &threads)
 	fmt.Printf("Ingresa la cantidad de datos que desea enviar: ")
@@ -35,10 +40,10 @@ func main() {
 	fmt.Printf("Ingresa la ruta del archivo: ")
 	fmt.Scanf("%s\n", &path)
 
-	readFile(path, threads, amount)
+	readFile(path, threads, amount, url)
 }
 
-func readFile(path string, threads int, amount int) {
+func readFile(path string, threads int, amount int, url string) {
 	// content : aquí guardaremos el contenido del archivo
 	content, err := ioutil.ReadFile("input.json")
 
@@ -63,44 +68,54 @@ func readFile(path string, threads int, amount int) {
 	}
 
 	cant := amount / threads
-	threadsNumber(persons, cant, threads)
+	threadsNumber(persons, cant, threads, url)
 }
 
-func threadsNumber(persons []Person, cant int, threads int) {
+func threadsNumber(persons []Person, cant int, threads int, url string) {
 	if threads == 1 {
-		mostrar(persons, 0, len(persons))
+		mostrar(persons, 0, len(persons), url)
 	} else if threads == 2 {
 		wg.Add(2)
-		go mostrar(persons, 0, cant)
-		go mostrar(persons, cant, len(persons))
+		go mostrar(persons, 0, cant, url)
+		go mostrar(persons, cant, len(persons), url)
 		wg.Wait()
 	} else if threads == 3 {
 		wg.Add(3)
-		go mostrar(persons, 0, cant)
-		go mostrar(persons, cant, cant*2)
-		go mostrar(persons, cant*2, len(persons))
+		go mostrar(persons, 0, cant, url)
+		go mostrar(persons, cant, cant*2, url)
+		go mostrar(persons, cant*2, len(persons), url)
 		wg.Wait()
 	} else if threads == 4 {
 		wg.Add(4)
-		go mostrar(persons, 0, cant)
-		go mostrar(persons, cant, cant*2)
-		go mostrar(persons, cant*2, cant*3)
-		go mostrar(persons, cant*3, len(persons))
+		go mostrar(persons, 0, cant, url)
+		go mostrar(persons, cant, cant*2, url)
+		go mostrar(persons, cant*2, cant*3, url)
+		go mostrar(persons, cant*3, len(persons), url)
 		wg.Wait()
 	} else if threads == 5 {
 		wg.Add(5)
-		go mostrar(persons, 0, cant)
-		go mostrar(persons, cant, cant*2)
-		go mostrar(persons, cant*2, cant*3)
-		go mostrar(persons, cant*3, cant*4)
-		go mostrar(persons, cant*4, len(persons))
+		go mostrar(persons, 0, cant, url)
+		go mostrar(persons, cant, cant*2, url)
+		go mostrar(persons, cant*2, cant*3, url)
+		go mostrar(persons, cant*3, cant*4, url)
+		go mostrar(persons, cant*4, len(persons), url)
 		wg.Wait()
 	}
 }
 
-func mostrar(persons []Person, init int, cant int) {
+func mostrar(persons []Person, init int, cant int, url string) {
 	for i := init; i < cant; i++ {
-		fmt.Println(persons[i].Nombre)
+		// Convertimos a json el objeto en turno
+		jsonData, _ := json.Marshal(persons[i])
+
+		// Hacemos la insersión en la ruta que nos dio el usuario y le mandamos el objeto json
+		response, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
+		if err != nil {
+			fmt.Printf("No se ha podido enviar la información: %s\n", err)
+		} else {
+			data, _ := ioutil.ReadAll(response.Body)
+			fmt.Println(data[0:0])
+		}
 		time.Sleep(time.Millisecond * 10)
 	}
 	wg.Done()
